e4d12d5999cbdc0a455cbff6bd94584fd7058dcf
Micheil Smith <micheil@brandedcode.com>
2011-01-25 08:42:21 +1100

    Added .git-ignore

---
+++ index.js
@@ -0,0 +1,114 @@
+var fs = require('fs'),
+    path = require('path');
+
+var AP = Array.prototype;
+
+
+
+/**
+ * Logger
+ *
+ * @constructor
+ * @param {String}      logpath        The file path to create/write to.
+**/
+function Logger(logpath) {
+  this.path = path.normalize(logpath);
+  this.stream = fs.createWriteStream(this.path, {
+    flags: 'a',
+    encoding: 'utf8',
+    mode: 0666
+  });
+
+  this.stream.write('\n');
+}
+
+
+/**
+ * Formats a message for logging
+ *
+ * @param {string}      level       The messages log level.
+ * @param {string}      message     The message to log.
+ * @return {string}     The formatted log message.
+**/
+Logger.prototype.format = function(level, message) {
+  return level + ' [' + (new Date()).toUTCString() + '] ' + message + '\n';
+};
+
+
+/**
+ * Writes a message to the log
+ *
+ * @param {string}      level       The level of the message being logged.
+ * @param {mixed...}    data        What to actually log.
+**/
+Logger.prototype.write = function(level, data) {
+  var args = AP.slice.call(arguments),
+      level = args.shift(),
+      message = '';
+
+  args.forEach(function(arg) {
+    message += ' ';
+    if (typeof arg === 'string') {
+      message += arg;
+    } else {
+      message += JSON.stringify(arg);
+    }
+  });
+
+  this.stream.write(this.format(level, message));
+};
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.log = function() {
+  this.write.apply(this, ['INFO'].concat(AP.slice.call(arguments)));
+};
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.info = Logger.prototype.log;
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.debug = function() {
+  this.write.apply(this, ['DEBUG'].concat(AP.slice.call(arguments)));
+};
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.warn = function() {
+  this.write.apply(this, ['WARN'].concat(AP.slice.call(arguments)));
+};
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.error = function() {
+  this.write.apply(this, ['ERROR'].concat(AP.slice.call(arguments)));
+};
+
+
+/**
+ * @see Logger.prototype.write
+**/
+Logger.prototype.fatal = function() {
+  this.write.apply(this, ['FATAL'].concat(AP.slice.call(arguments)));
+};
+
+
+
+/**
+ * Publicly export the Logger constructor
+ * @constructor
+ * @see Logger
+**/
+exports.Logger = Logger;
---
+++ test/test-logger.js
@@ -0,0 +1,60 @@
+var Logger = require('../').Logger,
+    assert = require('assert'),
+    fs = require('fs'),
+    path = require('path');
+
+var TEST_DIR = path.dirname(__filename);
+var LOG_PATH = path.join(TEST_DIR, './tmpdir/test-log');
+
+module.exports = {
+  'test: Logger.createLog()': function() {
+    var log = new Logger(LOG_PATH);
+    assert.ok(log instanceof Logger);
+    assert.equal(LOG_PATH, log.path);
+    assert.ok(log.stream instanceof fs.WriteStream);
+  },
+
+  'test: mylog.write': function() {
+    var log = new Logger(LOG_PATH);
+    var test_id = 0;
+
+    log.format = function(level, message) {
+      return level + ' [' + (new Date(0)).toUTCString() + '] ' + message + '\n';
+    };
+
+    log.stream.write = function(msg) {
+      switch (test_id) {
+        case 0:
+          assert.equal('INFO [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+        case 1:
+          assert.equal('INFO [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+        case 2:
+          assert.equal('DEBUG [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+        case 3:
+          assert.equal('WARN [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+        case 4:
+          assert.equal('ERROR [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+        case 5:
+          assert.equal('FATAL [Thu, 01 Jan 1970 00:00:00 GMT]  hello\n', msg);
+          break;
+      }
+    }
+
+    log.log('hello');
+    test_id++;
+    log.info('hello');
+    test_id++;
+    log.debug('hello');
+    test_id++;
+    log.warn('hello');
+    test_id++;
+    log.error('hello');
+    test_id++;
+    log.fatal('hello');
+  }
+};
